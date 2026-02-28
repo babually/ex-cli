8a4d6912cf8a41bd4ba39a1d58822aaceabdf38c
babually <mmrishoally@gmail.com>
2026-02-28 12:53:06 +0300

    fix the configuration for npx command creation

--- tsup.config.ts
+++ tsup.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
     entry: ['src/index.ts'],
-    format: ['esm', "cjs"],
+    format: ['esm'],
     dts: true,
     sourcemap: true,
     outExtension({ format }) {
-        return format === 'esm' ? { js: '.mjs' } : { js: '.cjs' };
+        return { js: '.mjs' };
     },
 })
